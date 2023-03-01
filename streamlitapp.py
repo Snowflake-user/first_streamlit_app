2154e79908a5b3f37d207fb2b0df1a9734a1ab0d
Snowflake-user <126448932+Snowflake-user@users.noreply.github.com>
2023-03-01 12:14:18 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -14,3 +14,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
+my_fruit_list=my_fruit_list.set_index('fruit)
